516f28d452baa2cd333fd3bda559453d2b52edfe
Trọng <petnhan17@gmail.com>
2026-09-23 00:22:15 +1000

    up

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,3 +1,11 @@
-import recipe from './recipe'
+import recipe from './recipe' // hoặc tên file món ăn của bạn (ví dụ: dish.js)
+import hashtag from './hashtag'
+import ingredient from './ingredient'
+import cookingMethod from './cookingMethod'
 
-export const schemaTypes = [recipe]
+export const schemaTypes = [
+  recipe,
+  hashtag,
+  ingredient,
+  cookingMethod,
+]
